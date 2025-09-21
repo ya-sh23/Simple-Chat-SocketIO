3a9e4872be1f8860c6643a410cc23d8b5bdc2d32
ya-sh23 <yashpatel231203@gmail.com>
2025-09-21 17:32:17 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+const app = express();
+
+const server = http.createServer(app);
+const PORT = 5000;
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  socket.on("userMessage", (message) => {
+    io.emit("message", { id: socket.id, message: message });
+  });
+});
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html", { root: __dirname });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
